bcd21b2431d8a346297b30f0a3753b9cd2c6c724
olka2g <ola2g@wp.pl>
2014-11-12 12:09:29 +0100

    Dalej początek tabu

--- Komiwojazer/TabuSearch.cpp
+++ Komiwojazer/TabuSearch.cpp
@@ -8,12 +8,46 @@ typedef struct TabuItem
 } TabuItem;
 
 
+typedef struct SolutionCandidate
+{
+	Route* modifiedRoutes;
+	int numberOfModifications;
+	Solution solution;
+} SolutionCandidate;
+
+bool shouldStop(int iteration)
+{
+	return (iteration < MAX_ITERATIONS);
+}
+
+void generateNeighbourhood(Solution baseSolution, std::vector<SolutionCandidate> neighbourhood)
+{
+	neighbourhood.clear();
+
+}
+
 
 Solution Tabu_findPath(CitiesData cities)
 {
 	int tabuSize = TABU_DURATION_TIME;
+	int i = 0;
 	TabuItem* tabuList = (TabuItem*) malloc(sizeof(TabuItem)*tabuSize);
+	std::vector<SolutionCandidate> neighbourhood;
 
+	Solution baseSolution = getRandomSolution(cities);
+	Solution bestSolution = baseSolution;
+	float bestCost = getRoutesLength(bestSolution);
+
+	while(!shouldStop(i))
+	{
+
+		generateNeighbourhood(baseSolution, neighbourhood);
+		//baseSolution = getBestSolution(neighbourhood, tabuList, tabuSize);
+
+		i++;
+	}
 
 	free(tabuList);
+
+	return bestSolution;
 }
--- Komiwojazer/TabuSearch.h
+++ Komiwojazer/TabuSearch.h
@@ -2,7 +2,9 @@
 #include "AlgorithmController.h"
 #include "DataTypes.h"
 #include <stdlib.h>
+#include <vector>
 
-#define TABU_DURATION_TIME 5;
+#define TABU_DURATION_TIME 5
+#define MAX_ITERATIONS 1000
 
 Solution Tabu_findPath(CitiesData cities);
